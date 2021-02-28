82a462509c8ecfb74e31176fdbc4900bd8b6b176
Daniel Schneider <daniel.schneider@microsoft.com>
2021-02-28 23:57:23 +0100

    add dask test

--- examples/dask/copy_nyc_taxi.py
+++ examples/dask/copy_nyc_taxi.py
@@ -1,6 +1,12 @@
 import uuid, time, os, json
 import io, sys, urllib.request
 from tqdm import tqdm
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--months", type=int, default=12)
+args = parser.parse_args()
+months = args.months
 
 cwd = os.getcwd()
 
@@ -14,7 +20,7 @@ if not os.path.exists(taxidir):
 
 filenames = []
 local_paths = []
-for i in range(1, 13): #13
+for i in range(1, months+1): 
     filename = "yellow_tripdata_2015-{month:02d}.csv".format(month=i)
     filenames.append(filename)
     
@@ -40,7 +46,7 @@ for idx, filename in enumerate(filenames):
         print("- File already exists locally")
 
 print("- Uploading taxi data... ")
-
+print("- az ml data upload -n nyctaxi -v 1 --path ./data/")
 os.system("az ml data upload -n nyctaxi -v 1 --path ./data/")
 
 print("- Data transfer complete")
